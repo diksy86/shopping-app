7e5182d458e84757c96096cf4ca77695ce3ad05b
Dijana R <dijana.radojevic86@gmail.com>
2021-11-22 17:02:44 +0100

    Updated products

--- src/catalogue.js
+++ src/catalogue.js
@@ -2,63 +2,63 @@ const catalogue = [
     {
         "id": 1,
         "name": "Christmas Tree",
-        "desc": "Decorated Pine Cone and Glitter Artificial Tabletop Christmas Tree",
+        "desc": "Decorated Tabletop Christmas Tree",
         "img": process.env.PUBLIC_URL + '/images/Decorated-Pine-Cone-and-Glitter-Artificial-Tabletop-Christmas-Tree.jpg',
         "price": 135.99
     },
     {
         "id": 2,
         "name": "Snow Globe Lantern",
-        "desc": "Musical Christmas Snow Globe Lantern Decoration",
+        "desc": "Musical Christmas Snow Globe Lantern",
         "img": process.env.PUBLIC_URL + '/images/Musical-Christmas-Snow-Globe-Lantern.jpg',
         "price": 57.99
     },
     {
         "id": 3,
         "name": "Christmas Lights",
-        "desc": "Solar LED Metal Christmas Tree Decoration Light",
+        "desc": "Solar LED Metal Christmas Tree Decoration",
         "img": process.env.PUBLIC_URL + '/images/Solar-LED-Metal-Christmas-Tree-Decoration-Light.jpg',
         "price": 39.99
     },
     {
         "id": 4,
         "name": "Candle Holder",
-        "desc": "Sparrows and Berries Flameless Glass Christmass Candle Holder",
+        "desc": "Sparrows and Berries XMAS Candle Holder",
         "img": process.env.PUBLIC_URL + '/images/Sparrows-and-Berries-Flameless-Glass-Christmas-Candle-Holder.jpg',
         "price": 15.49
     },
     {
         "id": 5,
         "name": "Christmas Ornaments",
-        "desc": "Winter White and Silver Christmas Ornaments",
+        "desc": "Winter White Christmas Ornaments",
         "img": process.env.PUBLIC_URL + '/images/Winter-White-and-Silver-Christmas-Ornaments.jpg',
         "price": 24.49
     },
     {
         "id": 6,
         "name": "Porch Decor",
-        "desc": "Lighted Round Wooden Christmas Porch Decor",
+        "desc": "Lighted Wooden Christmas Porch Decor",
         "img": process.env.PUBLIC_URL + '/images/Lighted-Round-Wooden-Christmas-Porch-Decor.jpg',
         "price": 124.49
     },
     {
         "id": 7,
         "name": "Outdoor Decoration",
-        "desc": "Merry Christmas Season Greetings Decoration",
+        "desc": "Merry Christmas Season Greetings",
         "img": process.env.PUBLIC_URL + '/images/Merry-Christmas-Season-Greetings.jpg',
         "price": 94.89
     },
     {
         "id": 8,
         "name": "Garden Xmas Lights",
-        "desc": "Solar Powered LED String Fairy Lights Xmas Decoration",
+        "desc": "Solar Powered LED String Fairy Lights",
         "img": process.env.PUBLIC_URL + '/images/Solar-Powered-LED-String-Fairy-Lights-Garden-Xmas.jpg',
         "price": 224.50
     },
     {
         "id": 9,
         "name": "Christmas Tree",
-        "desc": "Christmas Tree With Glitter Branches Red Berries Pinecones",
+        "desc": "Christmas Tree With Glitter Branches",
         "img": process.env.PUBLIC_URL + '/images/Christmas-Tree-with-Glitter-Branches-Red-Berries-Pinecones-by-Christopher-Knight-Home.jpg',
         "price": 124.50
     },
